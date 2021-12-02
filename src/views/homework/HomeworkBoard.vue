6a462ac3e1f339b0837cbdc5e24c02abd3bf9d86
sherrycen <1563922091@qq.com>
2021-12-02 20:58:10 +0800

    改了一点点

--- src/views/homework/HomeworkBoard.vue
+++ src/views/homework/HomeworkBoard.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
-        <head-top></head-top>       
+        <head-top></head-top>
+        <div style="margin: 20px 200px 0 40px">
+            <el-tabs :tab-position="tabPosition" style="height: 100%;">
+                <el-tab-pane v-for="(item,index) in hwlist" :key="index" :label="item">
+                    <div style="margin-left: 40px; margin-right: 40px">
+                        <p style="font-size:14px">请在此输入你的问题：</p>
+                        <el-input
+                        type="textarea"
+                        :rows="2"
+                        placeholder="请输入问题"
+                        v-model="textarea">
+                        </el-input>
+                    </div>
+                </el-tab-pane>
+            </el-tabs>
+        </div>       
     </div>
 </template>
 
@@ -9,7 +24,19 @@ import headTop from '../../components/HeadTop'
 export default {
     components: {
         headTop
-    }
+    },
+    data() {
+        return {
+            tabPosition:"left",
+            hwlist: [
+                '进程调度作业1',
+                '进程调度作业2',
+            ],
+            textarea: '',
+        }
+    },
+    methods: {
+    },
 }
 </script>
 
